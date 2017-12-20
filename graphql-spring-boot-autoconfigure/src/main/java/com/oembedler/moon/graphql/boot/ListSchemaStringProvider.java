3c5be431d3e693093ca464b529db1734538b31fc
Michiel Oliemans <oliemans@scopisto.com>
2017-12-20 23:32:30 +0100

    Added list schema provider and unit test for interface with impl

---
+++ graphql-spring-boot-autoconfigure/src/main/java/com/oembedler/moon/graphql/boot/ListSchemaStringProvider.java
@@ -0,0 +1,23 @@
+package com.oembedler.moon.graphql.boot;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListSchemaStringProvider implements SchemaStringProvider {
+
+  private final List<String> schemaStrings;
+
+  public ListSchemaStringProvider() {
+    schemaStrings = new ArrayList<>();
+  }
+
+  public void add(String schemaString) {
+    schemaStrings.add(schemaString);
+  }
+
+  @Override
+  public List<String> schemaStrings() throws IOException {
+    return schemaStrings;
+  }
+}
