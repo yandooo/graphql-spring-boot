80b16a8f8effae2af93cb2f99f026190338a1651
Michiel Oliemans <oliemans@scopisto.com>
2018-07-08 08:57:36 +0300

    Added example showing usage of ListSchemaStringProvider

--- graphql-spring-boot-autoconfigure/src/test/java/com/oembedler/moon/graphql/boot/test/graphqlJavaTools/GraphQLJavaToolsAutoConfigurationTest.java
+++ graphql-spring-boot-autoconfigure/src/test/java/com/oembedler/moon/graphql/boot/test/graphqlJavaTools/GraphQLJavaToolsAutoConfigurationTest.java
@@ -1,10 +1,15 @@
 package com.oembedler.moon.graphql.boot.test.graphqlJavaTools;
 
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.coxautodev.graphql.tools.SchemaParserDictionary;
 import com.oembedler.moon.graphql.boot.GraphQLJavaToolsAutoConfiguration;
+import com.oembedler.moon.graphql.boot.ListSchemaStringProvider;
+import com.oembedler.moon.graphql.boot.SchemaStringProvider;
 import com.oembedler.moon.graphql.boot.test.AbstractAutoConfigurationTest;
 import graphql.schema.GraphQLSchema;
 import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -29,4 +34,55 @@ public class GraphQLJavaToolsAutoConfigurationTest extends AbstractAutoConfigura
         Assert.assertNotNull(this.getContext().getBean(GraphQLSchema.class));
     }
 
+    @Configuration
+    static class InterfaceConfiguration {
+        class Query implements GraphQLQueryResolver {
+            Interface theInterface() {
+                return new Implementation();
+            }
+            Implementation theImplementation() {
+                return new Implementation();
+            }
+        }
+        interface Interface {
+            String method();
+        }
+        class Implementation implements Interface {
+            public String method() {
+                return "method";
+            }
+        }
+
+        @Bean
+        public SchemaStringProvider schemaStringProvider() {
+            ListSchemaStringProvider schemaStringProvider = new ListSchemaStringProvider();
+            schemaStringProvider.add("type Query {"
+                + "  theInterface:Interface!"
+                + "} "
+                + "interface Interface {"
+                + "  method:String!"
+                + "}"
+                + "type Implementation implements Interface {"
+                + "  method:String!"
+                + "}");
+            return schemaStringProvider;
+        }
+
+        @Bean
+        public Query query() {
+            return new Query();
+        }
+
+        @Bean
+        public SchemaParserDictionary schemaParserDictionary() {
+            return new SchemaParserDictionary().add(Implementation.class);
+        }
+    }
+
+    @Test
+    public void schemaWithInterfaceLoads() {
+        load(InterfaceConfiguration.class);
+
+        Assert.assertNotNull(this.getContext().getBean(GraphQLSchema.class));
+    }
 }
