4736bc6afa641c59108b956f4f1fed04e78fe0fc
Max Bruchmann <bruchmann@signal7.de>
2018-07-22 17:11:36 +0200

    Add graphiql-spring-boot-starter project
    - Move GraphQLTestUtils to graphiql-spring-boot-starter project
    - Use ObjectMapper to create test requests
    - Enable auto configuration
    - Add mutation show case

---
+++ graphql-spring-boot-starter-test/src/main/java/com/embedler/moon/graphql/testing/TestingConfiguration.java
@@ -0,0 +1,13 @@
+package com.embedler.moon.graphql.testing;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestingConfiguration {
+
+  @Bean
+  public GraphQLTestUtils graphQLTestUtils(){
+    return new GraphQLTestUtils();
+  }
+}
