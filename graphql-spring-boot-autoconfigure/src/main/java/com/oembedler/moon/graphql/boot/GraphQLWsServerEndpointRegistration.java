ccc2307ba7fe16335a4c89ba3c2e26752b2a0125
Andrew Potter <apottere@gmail.com>
2018-07-26 12:29:55 -0400

    Release 5.0.1

--- graphql-spring-boot-autoconfigure/src/main/java/com/oembedler/moon/graphql/boot/GraphQLWsServerEndpointRegistration.java
+++ graphql-spring-boot-autoconfigure/src/main/java/com/oembedler/moon/graphql/boot/GraphQLWsServerEndpointRegistration.java
@@ -1,6 +1,7 @@
 package com.oembedler.moon.graphql.boot;
 
 import graphql.servlet.GraphQLWebsocketServlet;
+import org.springframework.context.Lifecycle;
 import org.springframework.web.socket.server.standard.ServerEndpointRegistration;
 
 import javax.websocket.HandshakeResponse;
@@ -10,7 +11,7 @@ import javax.websocket.server.ServerEndpointConfig;
 /**
  * @author Andrew Potter
  */
-public class GraphQLWsServerEndpointRegistration extends ServerEndpointRegistration {
+public class GraphQLWsServerEndpointRegistration extends ServerEndpointRegistration implements Lifecycle {
 
     private final GraphQLWebsocketServlet servlet;
 
@@ -23,5 +24,19 @@ public class GraphQLWsServerEndpointRegistration extends ServerEndpointRegistrat
     public void modifyHandshake(ServerEndpointConfig sec, HandshakeRequest request, HandshakeResponse response) {
         super.modifyHandshake(sec, request, response);
         servlet.modifyHandshake(sec, request, response);
+    }
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void stop() {
+        servlet.beginShutDown();
+    }
+
+    @Override
+    public boolean isRunning() {
+        return !servlet.isShutDown();
     }
 }
