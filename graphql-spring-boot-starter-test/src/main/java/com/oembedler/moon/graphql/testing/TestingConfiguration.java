bd5a508f0b67be7b2130f5467af9e28cf224ae6f
Michiel Oliemans <oliemans@scopisto.com>
2018-07-28 22:43:05 +0200

    Fixed unit test due to too strict conditional on TestUtil

--- graphql-spring-boot-starter-test/src/main/java/com/oembedler/moon/graphql/testing/TestingConfiguration.java
+++ graphql-spring-boot-starter-test/src/main/java/com/oembedler/moon/graphql/testing/TestingConfiguration.java
@@ -1,17 +1,21 @@
 package com.oembedler.moon.graphql.testing;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
-@ConditionalOnWebApplication
-@ConditionalOnBean(TestRestTemplate.class)
+//@Profile("test")
+//@ConditionalOnClass(TestRestTemplate.class)
 public class TestingConfiguration {
 
     @Bean
+    @ConditionalOnMissingBean
     public GraphQLTestUtils graphQLTestUtils() {
         return new GraphQLTestUtils();
     }
