2a0442fca3de2e44c0c251a787d839b43eba1464
Michiel Oliemans <oliemans@scopisto.com>
2018-09-29 19:43:09 +0200

    Fix dependency setup

--- travis-build.sh
+++ travis-build.sh
@@ -5,4 +5,7 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ] &
     echo "Deploying release to Bintray"
     git checkout -f ${TRAVIS_BRANCH}
     ./gradlew clean assemble && ./gradlew check --info && ./gradlew bintrayUpload -x check --info
+else
+    echo "Verify"
+    ./gradlew clean assemble && ./gradlew check --info
 fi
