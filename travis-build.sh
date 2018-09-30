c5765c82a145bd6f6548b86e34a0f1526a6339bd
Michiel Oliemans <oliemans@scopisto.com>
2018-09-30 08:26:44 +0200

    Add release plugin configuration for automated versioning

--- travis-build.sh
+++ travis-build.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 set -ev
 
+saveGitCredentials() {
+    cat >$HOME/.netrc <<EOL
+machine github.com
+login ${GITHUB_USERNAME}
+password ${GITHUB_TOKEN}
+
+machine api.github.com
+login ${GITHUB_USERNAME}
+password ${GITHUB_TOKEN}
+EOL
+    chmod 600 $HOME/.netrc
+}
+
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ] && [ "${RELEASE}" = "true" ]; then
     echo "Deploying release to Bintray"
+    saveGitCredentials
     git checkout -f ${TRAVIS_BRANCH}
-    ./gradlew clean assemble && ./gradlew check --info && ./gradlew bintrayUpload -x check --info
+    ./gradlew clean assemble -Prelease.useAutomaticVersion=true && ./gradlew check --info
 else
     echo "Verify"
     ./gradlew clean assemble && ./gradlew check --info
