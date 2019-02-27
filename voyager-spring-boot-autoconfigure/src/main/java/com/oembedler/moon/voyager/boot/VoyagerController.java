94bee2514d5522948d7609d5a389b0ef87f57cf1
Aoudia <mf.aoudia@gmail.com>
2019-02-27 10:43:32 +0100

    feat(Voyager): Add get full static resources from CDNs

--- voyager-spring-boot-autoconfigure/src/main/java/com/oembedler/moon/voyager/boot/VoyagerController.java
+++ voyager-spring-boot-autoconfigure/src/main/java/com/oembedler/moon/voyager/boot/VoyagerController.java
@@ -19,42 +19,73 @@ import java.util.Map;
  * @author Guilherme Blanco
  */
 @Controller
-public class VoyagerController
-{
+public class VoyagerController {
+
+    private static final String CDNJS_CLOUDFLARE_COM_AJAX_LIBS = "//cdnjs.cloudflare.com/ajax/libs/";
+    private static final String CDN_JSDELIVR_NET_NPM = "//cdn.jsdelivr.net/npm/";
+    private static final String FETCH = "fetch";
+    private static final String ES_6_PROMISE = "es6-promise";
+    private static final String REACT = "react";
+    private static final String REACT_DOM = "react-dom";
+    private static final String VOYAGER = "voyager";
+
     @Value("${voyager.endpoint:/graphql}")
     private String graphqlEndpoint;
 
     @Value("${voyager.pageTitle:Voyager}")
     private String pageTitle;
 
+    @Value("${voyager.static.basePath:/}")
+    private String staticBasePath;
+
     @Value("${voyager.cdn.enabled:false}")
     private Boolean voyagerCdnEnabled;
 
-    @Value("${voyager.cdn.version:v1.x}")
+    @Value("${voyager.cdn.version:@1.0.0-rc.26}")
     private String voyagerCdnVersion;
 
     @RequestMapping(value = "${voyager.mapping:/voyager}")
-    public void voyager(HttpServletRequest request, HttpServletResponse response) throws IOException
-    {
+    public void voyager(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String contextPath = request.getContextPath();
         response.setContentType("text/html; charset=UTF-8");
-
-        String voyagerCssUrl = contextPath + "/vendor/voyager.css";
-        String voyagerJsUrl = contextPath + "/vendor/voyager.min.js";
-
-        if (voyagerCdnEnabled && StringUtils.isNotBlank(voyagerCdnVersion)) {
-            voyagerCssUrl = "//apis.guru/graphql-voyager/releases/" + voyagerCdnVersion + "/voyager.css";
-            voyagerJsUrl = "//apis.guru/graphql-voyager/releases/" + voyagerCdnVersion + "/voyager.min.js";
-        }
 
         String template = StreamUtils.copyToString(new ClassPathResource("voyager.html").getInputStream(), Charset.defaultCharset());
         Map<String, String> replacements = new HashMap<>();
         replacements.put("graphqlEndpoint", contextPath + graphqlEndpoint);
         replacements.put("pageTitle", pageTitle);
-        replacements.put("voyagerCssUrl", voyagerCssUrl);
-        replacements.put("voyagerJsUrl", voyagerJsUrl);
+        replacements.put("es6PromiseJsUrl", getCdnJsUrl(staticBasePath, voyagerCdnEnabled, ES_6_PROMISE,
+                "4.1.1", "es6-promise.auto.min.js"));
+        replacements.put("fetchJsUrl", getCdnJsUrl(staticBasePath, voyagerCdnEnabled, FETCH,
+                "3.0.0", "fetch.min.js"));
+        replacements.put("reactJsUrl", getCdnJsUrl(staticBasePath, voyagerCdnEnabled, REACT,
+                "16.8.3", "react.production.min.js"));
+        replacements.put("reactDomJsUrl", getCdnJsUrl(staticBasePath, voyagerCdnEnabled, REACT_DOM,
+                "16.8.3", "react-dom.production.min.js"));
+
+        replacements.put("voyagerCssUrl", getJsDeliverUrl(staticBasePath, voyagerCdnEnabled, VOYAGER,
+                voyagerCdnVersion, "dist/voyager.css", "voyager.css"));
+        replacements.put("voyagerJsUrl", getJsDeliverUrl(staticBasePath, voyagerCdnEnabled, VOYAGER,
+                voyagerCdnVersion, "dist/voyager.min.js", "voyager.worker.min.js"));
+        replacements.put("voyagerWorkerJsUrl", getJsDeliverUrl(staticBasePath, voyagerCdnEnabled, VOYAGER,
+                voyagerCdnVersion, "dist/voyager.worker.min.js", "voyager.worker.min.js"));
         replacements.put("contextPath", contextPath);
 
         response.getOutputStream().write(StrSubstitutor.replace(template, replacements).getBytes(Charset.defaultCharset()));
+    }
+
+    private String getCdnJsUrl(String staticBasePath, Boolean isCdnEnabled, String library,
+                               String cdnVersion, String filename) {
+        if (isCdnEnabled && StringUtils.isNotBlank(cdnVersion)) {
+            return CDNJS_CLOUDFLARE_COM_AJAX_LIBS + library + cdnVersion + "/" + filename;
+        }
+        return staticBasePath + "vendor/" + filename;
+    }
+
+    private String getJsDeliverUrl(String staticBasePath, Boolean isCdnEnabled, String library,
+                                   String cdnVersion, String cdnFileName, String filename) {
+        if (isCdnEnabled && StringUtils.isNotBlank(cdnVersion)) {
+            return CDN_JSDELIVR_NET_NPM + library + "/" + cdnVersion + "/" + cdnFileName;
+        }
+        return staticBasePath + "vendor/" + filename;
     }
 }
