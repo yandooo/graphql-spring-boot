065db798f892546d2edbaf62aa137bc9abefaccb
Michiel Oliemans <oliemans@scopisto.com>
2020-04-01 18:25:55 +0200

    Upgrade build script for snapshot deployments

--- travis-build.sh
+++ travis-build.sh
@@ -14,11 +14,16 @@ EOL
     chmod 600 $HOME/.netrc
 }
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ] && [ "${RELEASE}" = "true" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
+  if [ "${RELEASE}" = "true" ]; then
     echo "Deploying release to Bintray"
     saveGitCredentials
-    git checkout -f ${TRAVIS_BRANCH}
     ./gradlew clean assemble && ./gradlew check --info && ./gradlew bintrayUpload -x check --info
+  else
+    echo "Deploying snapshot"
+    saveGitCredentials
+    ./gradlew artifactoryPublish -Dsnapshot=true -Dbuild.number="${TRAVIS_BUILD_NUMBER}"
+  fi
 else
     echo "Verify"
     ./gradlew clean assemble && ./gradlew check --info
