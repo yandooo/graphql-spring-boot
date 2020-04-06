62156788c71817c8c2434f174a16c11dff412051
Balázs Vajner <vajner.balazs@gmail.com>
2020-04-06 19:42:17 +0200

    test: add tests for test facilities auto configuration

---
+++ graphql-spring-boot-test-autoconfigure/src/test/java/com/graphql/spring/boot/test/GraphQLTestSubscriptionAutoConfigurationDefaultConfigTest.java
@@ -0,0 +1,14 @@
+package com.graphql.spring.boot.test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Testing the auto-configuration of the GraphQLTestSubscription bean / default settings.")
+public class GraphQLTestSubscriptionAutoConfigurationDefaultConfigTest extends GraphQLTestAutoConfigurationTestBase {
+
+    @Test
+    @DisplayName("Should provide a GraphQLTestTemplate bean.")
+    void shouldProvideGraphQLTestSubscriptionBean() {
+        assertThatTestSubscriptionWorksCorrectly();
+    }
+}
---
+++ graphql-spring-boot-test-autoconfigure/src/test/java/com/graphql/spring/boot/test/beans/TestQuery.java
@@ -0,0 +1,12 @@
+package com.graphql.spring.boot.test.beans;
+
+import graphql.kickstart.tools.GraphQLQueryResolver;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestQuery implements GraphQLQueryResolver {
+
+    public String testQuery() {
+        return "foo";
+    }
+}
