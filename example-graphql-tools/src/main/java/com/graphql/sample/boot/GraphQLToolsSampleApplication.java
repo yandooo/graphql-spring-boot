ed258adbf060d987de8d2aaaac2d8d30bee57f69
Michiel Oliemans <oliemans@scopisto.com>
2020-09-06 10:21:58 +0200

    Remove obsolete use of JavaTimeModule fix #415

--- example-graphql-tools/src/main/java/com/graphql/sample/boot/GraphQLToolsSampleApplication.java
+++ example-graphql-tools/src/main/java/com/graphql/sample/boot/GraphQLToolsSampleApplication.java
@@ -1,10 +1,7 @@
 package com.graphql.sample.boot;
 
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import graphql.kickstart.execution.config.GraphQLServletObjectMapperConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GraphQLToolsSampleApplication {
@@ -13,8 +10,4 @@ public class GraphQLToolsSampleApplication {
         SpringApplication.run(GraphQLToolsSampleApplication.class, args);
     }
 
-    @Bean
-    public GraphQLServletObjectMapperConfigurer objectMapperConfigurer() {
-        return (mapper -> mapper.registerModule(new JavaTimeModule()));
-    }
 }
