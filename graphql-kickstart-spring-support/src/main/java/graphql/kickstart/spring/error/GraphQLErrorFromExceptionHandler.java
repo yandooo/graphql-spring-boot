09d1230ff025359704271f35219974cfb7fe6a0f
Tomasz Fijalkowski <tomasz.fijalkowski@allegro.pl>
2020-10-06 09:48:28 +0200

    Add extensions data to ErrorContext class
    (cherry picked from commit e15cc8bb0c32a9f477fbeca65b8d2ed57dbbbd00)

--- graphql-kickstart-spring-support/src/main/java/graphql/kickstart/spring/error/GraphQLErrorFromExceptionHandler.java
+++ graphql-kickstart-spring-support/src/main/java/graphql/kickstart/spring/error/GraphQLErrorFromExceptionHandler.java
@@ -32,7 +32,13 @@ class GraphQLErrorFromExceptionHandler extends DefaultGraphQLErrorHandler {
   }
 
   private Collection<GraphQLError> transform(GraphQLError error) {
-    return extractException(error).map(this::transform)
+    ErrorContext errorContext = new ErrorContext(
+            error.getLocations(),
+            error.getPath(),
+            error.getExtensions(),
+            error.getErrorType()
+    );
+    return extractException(error).map(throwable -> transform(throwable, errorContext))
         .orElse(singletonList(new GenericGraphQLError(error.getMessage())));
   }
 
@@ -47,13 +53,13 @@ class GraphQLErrorFromExceptionHandler extends DefaultGraphQLErrorHandler {
     return Optional.empty();
   }
 
-  private Collection<GraphQLError> transform(Throwable throwable) {
+  private Collection<GraphQLError> transform(Throwable throwable, ErrorContext errorContext) {
     Map<Class<? extends Throwable>, GraphQLErrorFactory> applicables = new HashMap<>();
     factories.forEach(factory -> factory.mostConcrete(throwable).ifPresent(t -> applicables.put(t, factory)));
     return applicables.keySet().stream()
         .min(new ThrowableComparator())
         .map(applicables::get)
-        .map(factory -> factory.create(throwable))
+        .map(factory -> factory.create(throwable, errorContext))
         .orElse(singletonList(new ThrowableGraphQLError(throwable)));
   }
 
