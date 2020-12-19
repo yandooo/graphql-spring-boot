89ed1d76a166803e9f28645b15e097bad7226edb
Michiel Oliemans <oliemans@scopisto.com>
2020-12-19 21:56:34 +0100

    Fix data loader customer example

--- example-request-scoped-dataloader/src/main/java/graphql/servlet/examples/dataloader/requestscope/CustomerResolver.java
+++ example-request-scoped-dataloader/src/main/java/graphql/servlet/examples/dataloader/requestscope/CustomerResolver.java
@@ -3,19 +3,22 @@ package graphql.servlet.examples.dataloader.requestscope;
 import graphql.kickstart.tools.GraphQLResolver;
 import graphql.kickstart.execution.context.GraphQLContext;
 import graphql.schema.DataFetchingEnvironment;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import org.dataloader.DataLoader;
+import org.dataloader.DataLoaderRegistry;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomerResolver implements GraphQLResolver<Customer> {
 
   public CompletableFuture<String> getName(Customer customer, DataFetchingEnvironment dfe) {
-    final DataLoader<Integer, String> dataloader = ((GraphQLContext) dfe.getContext())
-        .getDataLoaderRegistry()
-        .map(it -> it.getDataLoader("customerDataLoader"))
-        .map(it -> it.load(customer.getCustomerId()))
-        .orElse(null);
+    Optional<DataLoaderRegistry> registry = ((GraphQLContext) dfe.getContext()).getDataLoaderRegistry();
+    if (registry.isPresent()) {
+      DataLoader<Integer, String> customerLoader = registry.get().getDataLoader("customerDataLoader");
+      return customerLoader.load(customer.getCustomerId());
+    }
+    throw new IllegalStateException("No customer data loader found");
   }
 
 }
